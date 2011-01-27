3db9a04f9cf22d0a0a713d2f63d03d9923c8773e
Florian Sprenger <f.sprenger@4teamwork.ch>
2011-01-27 10:06:23 +0100

    moved column width css to the main template to avoid caching

---
+++ izug/basetheme/skins/izug_basetheme_templates/getContentColumnWidth.py
@@ -0,0 +1,9 @@
+## Script (Python) "getContentColumnWidth"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=
+return "71em"
---
+++ izug/basetheme/skins/izug_basetheme_templates/getLeftColumnWidth.py
@@ -0,0 +1,9 @@
+## Script (Python) "getLeftColumnWidth"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=
+return "25em"
