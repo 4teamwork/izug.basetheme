00138e188b3b2b37cbb19af76ac5e424a6dfe6b0
Florian Sprenger <f.sprenger@4teamwork.ch>
2011-02-04 13:36:06 +0100

    Reactivated the custom code for breadcrumbs; if the current user has no rights to view an item, it is not clickable

--- izug/basetheme/browser/navigation.py
+++ izug/basetheme/browser/navigation.py
@@ -2,34 +2,26 @@ from Acquisition import aq_inner
 from zope.interface import implements
 from zope.component import getMultiAdapter
 
-from Products.CMFCore.interfaces import IContentish
 from Products.CMFCore.utils import _checkPermission
 from Products.CMFPlone import utils
 from Products.CMFPlone.browser.interfaces import INavigationBreadcrumbs
 from Products.CMFPlone.interfaces import IHideFromBreadcrumbs
 from plone.app.layout.navigation.root import getNavigationRoot
 from Products.CMFPlone.browser.navigation import get_view_url
+from zope.publisher.browser import BrowserView
 
 
-class PhysicalNavigationBreadcrumbs(grok.View):
+class PhysicalNavigationBreadcrumbs(BrowserView):
     """ Breadcrumbs for navigation.
         If the user does not have 'View' permission on the item do not
         provide an url, just the title.
     """
-    grok.name('breadcrumbs_view')
-    grok.context(IContentish)
-    grok.require('zope2.View')
     implements(INavigationBreadcrumbs)
     
-    def render(self):
-        """ required by grok """
-        return
-
     def breadcrumbs(self):
         context = aq_inner(self.context)
         request = self.request
         container = utils.parent(context)
-
         try:
             name, item_url = get_view_url(context)
         except AttributeError:
