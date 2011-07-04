edacc9a4f3b764cc5c148c3e34f79cee4df7bf82
Florian Sprenger <f.sprenger@4teamwork.ch>
2011-07-04 16:59:26 +0200

    added script from zugskin

---
+++ izug/basetheme/skins/izug_zugch/addZugMerklisteToHomeFolder.py
@@ -0,0 +1,61 @@
+## Script (Python) "addZugMerklisteToHomeFolder"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=
+##title=
+##
+from Products.CMFPlone import PloneMessageFactory as _
+
+
+plone_root = context.portal_url.getPortalObject()
+mtool = context.portal_membership
+isAnon = mtool.isAnonymousUser()
+
+state = context.restrictedTraverse("@@plone_context_state")
+view_url_backup = '%s/%s' % (context.absolute_url(), state.view_template_id())
+
+view_url = context.REQUEST.get('HTTP_REFERER', view_url_backup) +'#portal-message'
+
+RESPONSE = context.REQUEST.RESPONSE
+putils = context.plone_utils
+
+#make sure we are not on portal root
+if context == plone_root:
+  #XXX replace with a status message
+  putils.addPortalMessage(_(u'merkliste_message_root',default=u'You can not use this action on plone root'), 'error')
+  return RESPONSE.redirect(view_url)
+
+if isAnon:
+  putils.addPortalMessage(_(u'merkliste_message_register',default=u'only registered user can use this function.'), 'error')
+  return RESPONSE.redirect(view_url)
+
+
+objectUID = context.UID()
+title = context.pretty_title_or_id
+
+#get members home folder
+member = mtool.getAuthenticatedMember()
+if not member:
+  putils.addPortalMessage(_(u'merkliste_message_admin',default=u'you are a Portel Administrator, without user profil'), 'error')
+  return RESPONSE.redirect(view_url)
+
+homefolder = member.getHomeFolder()
+
+#create the object
+
+#get the created Object
+try:
+  id ='fav_' + str(int( context.ZopeTime()))
+  newid = homefolder.invokeFactory(id=id,type_name='ZugMerkliste',favObj=objectUID)
+  homefolder[newid].setTitle(context.Title())
+  
+
+except:
+  putils.addPortalMessage(_(u'merkliste_message_somereason',default=u'for some reasons the object could not be created, it is possible your homefolder does no exist'), 'error')
+  return RESPONSE.redirect(view_url)
+
+putils.addPortalMessage(_(u'merkliste_message_added',default=u'added to Merkliste'),'info')
+return RESPONSE.redirect(view_url)
