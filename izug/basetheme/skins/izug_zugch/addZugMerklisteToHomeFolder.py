76fcd2df5548b3afa2cf35257a5d73308e201ff0
Florian Sprenger <f.sprenger@4teamwork.ch>
2011-07-05 16:25:04 +0200

    added UID to redirect url in addZugMerklisteToHomeFolder.py to avoid caching

--- izug/basetheme/skins/izug_zugch/addZugMerklisteToHomeFolder.py
+++ izug/basetheme/skins/izug_zugch/addZugMerklisteToHomeFolder.py
@@ -17,30 +17,30 @@ isAnon = mtool.isAnonymousUser()
 state = context.restrictedTraverse("@@plone_context_state")
 view_url_backup = '%s/%s' % (context.absolute_url(), state.view_template_id())
 
-view_url = context.REQUEST.get('HTTP_REFERER', view_url_backup) +'#portal-message'
+view_url = context.REQUEST.get('HTTP_REFERER', view_url_backup) #+'#portal-message'
 
 RESPONSE = context.REQUEST.RESPONSE
 putils = context.plone_utils
 
-#make sure we are not on portal root
 if context == plone_root:
   #XXX replace with a status message
   putils.addPortalMessage(_(u'merkliste_message_root',default=u'You can not use this action on plone root'), 'error')
   return RESPONSE.redirect(view_url)
 
-if isAnon:
-  putils.addPortalMessage(_(u'merkliste_message_register',default=u'only registered user can use this function.'), 'error')
-  return RESPONSE.redirect(view_url)
-
-
 objectUID = context.UID()
 title = context.pretty_title_or_id
+
+if isAnon:
+  putils.addPortalMessage(_(u'merkliste_message_register',default=u'only registered user can use this function.'), 'error')
+  return RESPONSE.redirect(view_url + "#" + objectUID)
+
+
 
 #get members home folder
 member = mtool.getAuthenticatedMember()
 if not member:
   putils.addPortalMessage(_(u'merkliste_message_admin',default=u'you are a Portel Administrator, without user profil'), 'error')
-  return RESPONSE.redirect(view_url)
+  return RESPONSE.redirect(view_url + "#" + objectUID)
 
 homefolder = member.getHomeFolder()
 
@@ -55,7 +55,7 @@ try:
 
 except:
   putils.addPortalMessage(_(u'merkliste_message_somereason',default=u'for some reasons the object could not be created, it is possible your homefolder does no exist'), 'error')
-  return RESPONSE.redirect(view_url)
+  return RESPONSE.redirect(view_url  + "#" + objectUID)
 
 putils.addPortalMessage(_(u'merkliste_message_added',default=u'added to Merkliste'),'info')
-return RESPONSE.redirect(view_url)
+return RESPONSE.redirect(view_url  + "#" + objectUID)
