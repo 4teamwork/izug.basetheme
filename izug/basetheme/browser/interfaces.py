79c99218051a4094f4cfda536ff0e932414cffe7
Elio Schmutz <e.schmutz@4teamwork.ch>
2011-08-11 11:48:45 +0200

    added opengever specific browserlayer: izug.basetheme.opengever

--- izug/basetheme/browser/interfaces.py
+++ izug/basetheme/browser/interfaces.py
@@ -3,3 +3,8 @@ from plone.theme.interfaces import IDefaultPloneLayer
 class IThemeSpecific(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 browser layer.
     """
+
+class IOpengeverSpecific(IDefaultPloneLayer):
+    """Marker interface that defines a Zope 3 browser layer specific for OpenGever.
+    """
+
