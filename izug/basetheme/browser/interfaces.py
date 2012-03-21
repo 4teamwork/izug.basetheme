dd49cf9fcee8d1d5c5e31eb36060bde5079d8a2f
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2012-03-21 10:57:37 +0100

    register browserlayer for directories

--- izug/basetheme/browser/interfaces.py
+++ izug/basetheme/browser/interfaces.py
@@ -6,14 +6,21 @@ class IThemeSpecific(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 skin layer.
     """
 
+
 class IOpengeverSpecific(Interface):
     """Marker interface that defines a Zope 3 browser layer specific for OpenGever.
     """
+
 
 class IIzugSpecific(Interface):
     """Marker interface that defines a Zope 3 browser layer specific for izug.
     """
 
+
 class IZugSpecific(Interface):
     """Marker interface that defines a Zope 3 browser layer specific for zug.
     """
+
+
+class IDirectorySpecific(Interface):
+    """Marker Interface for directories"""
