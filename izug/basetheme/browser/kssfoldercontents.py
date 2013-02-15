51ce6e6f3510685bf218bfec3b3ada792f6261b4
Jonas Baumann <jone@jone.ch>
2013-02-15 15:40:35 +0100

    Remove folder contents customizations.

--- izug/basetheme/browser/kssfoldercontents.py
+++
@@ -1,6 +0,0 @@
-from izug.basetheme.browser.foldercontents import IzugFolderContentsTable
-from plone.app.content.browser.tableview import TableKSSView
-
-
-class FolderContentsKSSView(TableKSSView):
-    table = IzugFolderContentsTable
